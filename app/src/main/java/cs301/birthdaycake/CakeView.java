55f298cb374de48ab9181642725ebe3b933557f7
junkin22 <junkins22@up.edu>
2020-09-03 17:19:49 -0700

    Lab 2 Progress

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -120,7 +120,7 @@ public class CakeView extends SurfaceView {
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
         //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/1 - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft + 2*cakeWidth/3 - candleWidth/2, cakeTop);
 
         //new Candle
         drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/2, cakeTop);
